7b2a03b09f0118fd95d0eefd8e8eaa2b6f93cd24
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-07 18:28:29 +0530

    added antd and antd-icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,25 @@
 'use client';
-import React from "react";
-import PropTypes from "prop-types";
-import "./globals.css";
-
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import ErrorBoundary from '../components/ErrorBoundary';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+// import NextTopLoader from 'nextjs-toploader';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <AntdRegistry>
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary>
+        </AntdRegistry>
       </body>
     </html>
   );
 }
+
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired, // Ensure children is a node and is required
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,22 +24,7 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
@@ -48,50 +33,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
