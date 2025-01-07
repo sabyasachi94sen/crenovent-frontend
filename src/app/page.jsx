db704051d3dfad70189718485f3cd5ec8a7f71ee
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-07 17:19:13 +0530

    2nd commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const x='';
+  const xz='';
   return (
     <div className={''}>
       <main className={styles.main}>
