9200f6fbdfd6c1d2143ea6eb28d091267385ab58
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-08 18:13:06 +0530

    updated components

--- src/components/BookNowForm.jsx
+++ src/components/BookNowForm.jsx
@@ -12,7 +12,7 @@ export const BookNowForm = () => {
     return (
         <Form layout="vertical" scrollToFirstError={true} onFinish={handleSubmit}>
             <div className="row">
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                     <Form.Item name='name' label="Name" rules={[{ required: true, message: 'Please input your name!' }]} >
                         <Input
                             width={'100%'}
@@ -20,7 +20,7 @@ export const BookNowForm = () => {
                         />
                     </Form.Item>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                     <Form.Item name='phone' label="Mobile" rules={[{ required: true, message: 'Please input your mobile!' }]}>
                         <Input
                             type="number"
@@ -29,7 +29,7 @@ export const BookNowForm = () => {
                         />
                     </Form.Item>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                     <Form.Item name='email' label="Email" rules={[{ required: true, message: 'Please input your email!' },
 
                     () => ({
--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,22 +1,60 @@
 'use client'
 
-import React from "react";
-import {Image} from 'antd'
-import {BookNowForm} from "./BookNowForm";
+import React, { useEffect } from "react";
+import { Image } from 'antd'
+import { BookNowForm } from "./BookNowForm";
 
-export const ProfileView=()=>{
+export const ProfileView = () => {
+
+
+    useEffect(() => {
+        function animateValue(obj, start, end, duration) {
+            let startTimestamp = null;
+            const step = (timestamp) => {
+                if (!startTimestamp) startTimestamp = timestamp;
+                const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                obj.innerHTML = Math.floor(progress * (end - start) + start);
+                if (progress < 1) {
+                    window.requestAnimationFrame(step);
+                }
+            };
+            window.requestAnimationFrame(step);
+        }
+
+        const calls = document.getElementById("calls");
+        const languages=document.getElementById("languages");
+        animateValue(calls, 0, 96, 1000);
+        animateValue(languages, 0, 16, 1000);
+        
+    }, [])
+
+
     return (
         <div className="row pt-5 fade-in">
-        <div className="col-lg-6 profile-view">
-            <p className="txt-danger h-46 semibold">Leading the Future of Sales with AI Expertise</p>
-            <p className="h-26 bold">Transforming Customer Service through Data Excellence</p>
-            <p className="h-16 regular txt-secondary mt-4 lh-lg">Don’t let inadequate resources and missed opportunities hinder  your contact center operations and customer experience
-            Drive maximum potential of your business</p>
-            <BookNowForm />
-        </div>
-        <div className="col-lg-6 profile-view">
-            <Image preview={false} width={'100%'} src="/img/chatbox.webp" alt="chatbox" />
-        </div>
+            <div className="col-lg-6 profile-view">
+                <p className="txt-danger h-46 semibold">Leading the Future of Sales with AI Expertise</p>
+                <p className="h-26 bold">Transforming Customer Service through Data Excellence</p>
+                <p className="h-16 regular txt-secondary mt-4 lh-lg">Don’t let inadequate resources and missed opportunities hinder  your contact center operations and customer experience
+                    Drive maximum potential of your business</p>
+                <BookNowForm />
+                <div className="mt-5 d-flex gap-5 flex-wrap">
+                    <div>
+                        <span className="h-46 semibold"><span id='calls'>0</span>Mn</span>
+                        <div className="h-16 regular txt-secondary mt-1" >
+                            Calls Handled by AI Agents/Month
+                        </div>
+                    </div>
+                    <div>
+                        <span className="h-46 semibold"><span id='languages'>0</span>+</span>
+                        <div className="h-16 regular txt-secondary mt-1" >
+                        Languages Supported
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-6 profile-view">
+                <Image preview={false} width={'100%'} src="/img/chatbox.webp" alt="chatbox" />
+            </div>
         </div>
     )
 }
