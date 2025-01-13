813b21c9da382fde429709e947f0be8f87aaf42a
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-13 17:25:32 +0530

    updated new page again

--- src/utils/data.js
+++ src/utils/data.js
@@ -101,7 +101,7 @@ export const data={
                 { year: '1996', value: 6 },
                 { year: '1997', value: 7 },
                 { year: '1998', value: 9 },
-                { year: '1999', value: 13 },
+                { year: '1999', value: 4 },
               ]
               ,
                title: {
